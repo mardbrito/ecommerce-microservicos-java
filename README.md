6cb7d53124ab16133e261f61ed849459bc9f2cb9
mardbrito <marcelo4web@gmail.com>
2021-10-07 20:34:44 -0400

    first commit of project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecommerce-microservicos-java
+
+Projeto prático de uma solução e-commerce com a arquitetura de microsserviços com Java utilizando a stack do Spring (Spring Boot, Spring Cloud Streams).
---
+++ checkout/src/main/java/br/brito/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,14 @@
+package br.brito.ecommerce.checkout.config;
+
+import com.hatanaka.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import com.hatanaka.ecommerce.checkout.streaming.PaymentPaidSink;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
